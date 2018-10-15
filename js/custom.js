86a0ed5928cfef1c5c9eee176f77cc8ba445def0
Marina Rumyantsev <aniramrum@gmail.com>
2018-10-16 01:02:46 +0300

    Fields validation

--- js/custom.js
+++ js/custom.js
@@ -10,33 +10,92 @@ if (matchMedia('(max-width: 991px)').matches) {
 /*===================
 FORM FIELDS VALIDATION
 ===================== */
-function validateField(field) {
-    if ($(field.id) == 'email') {
-        console.log($(this.id));
-        if (isValidEmail($(field).val())) {
-            $(this).removeClass('notValidField');
-            $('.notValidEmail').show();
-        } else {
-            $('.notValidEmail').show();
-            $(this).addClass('notValidField');
-        }
 
+$('input, textarea').blur(function() {
+    var currentField = $(this);
+    validateField(currentField);
+});
+
+function validateField(field) {
+
+
+    // TODO return value
+    var validationMessage = '';
+
+    if ($(field).attr('id') == 'email') {
+
+        validationMessage = '.notValidEmail';
+
+        if ($(field).val().length > 0) {
+
+            if (isValidEmail($(field).val())) {
+                console.log('Email is valid');
+                defineFieldAsValid($(field), validationMessage);
+            } else {
+                console.log('Email is not valid');
+                defineFieldAsNotValid($(field), validationMessage);
+            }
+        }
     }
+
+
+    if ($(field).attr('id') == 'name') {
+
+        validationMessage = '.notValidName';
+
+        if ($(field).val().length < 1) {
+            console.log('Name is not valid');
+            defineFieldAsNotValid($(field), validationMessage);
+        } else {
+            console.log('Name is valid');
+            defineFieldAsValid($(field), validationMessage);
+        }
+    }
+
+
+    if ($(field).attr('id') == 'message') {
+
+        validationMessage = '.notValidMessage';
+
+        if ($(field).val().length < 1) {
+
+            console.log('message is not valid');
+            defineFieldAsNotValid($(field), validationMessage);
+        } else {
+
+            console.log('message is valid');
+            defineFieldAsValid($(field), validationMessage);
+        }
+    }
+
 };
 
-$('input').blur(function() {
-    // if ($(this).hasClass('notValidField')) {
-    console.log($(this));
-    console.log($(this.id));
+function defineFieldAsValid(field, validationMessage) {
+    $(field).removeClass('notValidField');
+    $(validationMessage).hide();
+}
 
-    validateField($(this.id));
-    // }
-});
+function defineFieldAsNotValid(field, validationMessage) {
+    $(validationMessage).show();
+    $(field).addClass('notValidField');
+}
 
 function isValidEmail(emailAddress) {
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
     return pattern.test(emailAddress);
 };
+
+function validateFields(fieldsArray) {
+
+    var isValid = true;
+
+    $.each(fieldsArray, function(index, value) {
+        if (!validateField(value)) {
+            isValid = false;
+        }
+    });
+    return isValid;
+}
 
 
 /* =================================
@@ -44,31 +103,13 @@ function isValidEmail(emailAddress) {
 =================================== */
 $("#contact").submit(function(e) {
     e.preventDefault();
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var subject = $("#subject").val();
-    var message = $("#message").val();
-    var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
+    var name = $("#name");
+    var email = $("#email");
+    var subject = $("#subject");
+    var message = $("#message");
+    var dataString = 'name=' + name.val() + '&email=' + email.val() + '&subject=' + subject.val() + '&message=' + message.val();
 
-
-
-    function validate(email, message, name) {
-
-        if (!isValidEmail(email)) {
-            $('.notValidEmail').show();
-            $("#email").addClass('notValidField');
-        }
-        if (!(message.length > 1)) {
-            $('.notValidMessage').show();
-            $("#message").addClass('notValidField');
-        }
-        if (!(name.length > 1)) {
-            $('.notValidName').show();
-            $("#name").addClass('notValidField');
-        }
-    }
-
-    if (validate(email, message, name)) {
+    if (validateFields([email, message, name])) {
         $.ajax({
             type: "POST",
             url: "sendmail.php",
@@ -86,6 +127,8 @@ $("#contact").submit(function(e) {
             }
         });
     } else {
+
+        console.log('not sent');
         $('.error').fadeIn(1000);
         $('.success').fadeOut(500);
     }
